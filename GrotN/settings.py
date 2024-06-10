06fc0e909c6db748e883509496ce2ac31d0176e6
Alp Niksarli <alp.niksarli@gmail.com>
2024-06-10 20:25:44 +0300

    changed sqlite path for vercel deployment

--- GrotN/settings.py
+++ GrotN/settings.py
@@ -88,16 +88,17 @@ WSGI_APPLICATION = 'GrotN.wsgi.application'
 # PRODUCTION
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        # VERCEL PRODUCTION (not working)
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST'),
+        # 'PORT': os.getenv('DB_PORT', '5432'),
 
         # DEVELOPMENT
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
 
     }
 }
